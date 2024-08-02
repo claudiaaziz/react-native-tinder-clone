ea890b4610fb48b9beff8940ac3831a67fb754db
claudiaaziz <claudiahannahh@gmail.com>
2024-08-02 14:06:35 -0400

    React native setup with react-native-cli

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@react-native-community',
+        // other configurations you might have
+    ],
+    rules: {
+        'prettier/prettier': 'off',
+    },
+};
