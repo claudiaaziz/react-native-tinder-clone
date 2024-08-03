9bd09ad453b1e6e8a6bc516e838dfe13554ab0cf
claudiaaziz <claudiahannahh@gmail.com>
2024-08-03 10:00:21 -0400

    Work on card swipe animations using gestures

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Card from './src/components/Card';
 import users from './assets/data/users';
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    withSpring,
+} from 'react-native-reanimated';
+import { GestureDetector, Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
+    const values = useSharedValue(1);
+
+    const cardStyle = useAnimatedStyle(() => ({
+        opacity: values.value,
+    }));
+
     return (
-        <View style={styles.container}>
-            {users.map((user) => (
-                <Card user={user} />
-            ))}
-        </View>
+        <GestureHandlerRootView style={styles.container}>
+            <View style={styles.container}>
+                {/* {users.map((user) => ( */}
+                <Animated.View style={[styles.animatedCard, cardStyle]}>
+                    <Card user={users[0]} />
+                </Animated.View>
+                <Pressable
+                    onPress={() =>
+                        (values.value = withSpring(Math.random()))
+                    }
+                >
+                    <Text>update value</Text>
+                </Pressable>
+                {/* ))} */}
+            </View>
+        </GestureHandlerRootView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    animatedCard: {
+        width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
     },
